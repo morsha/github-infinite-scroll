da900e01f4f8f0141c7986d96b138b10bf4b4a99
KevinKT <lovemdjk@gmail.com>
2021-08-29 23:44:59 +0800

    feature: add details to ScrollItem

--- src/components/InfiniteScroll/ScrollItem.js
+++ src/components/InfiniteScroll/ScrollItem.js
@@ -6,9 +6,14 @@ const GITHUB_DOMAIN = 'https://github.com';
 
 type Props = {
   itemData: {
-    full_name: String,
-    name: String,
-    description: String,
+    full_name?: String,
+    name?: String,
+    description?: String,
+    stargazers_count?: Number,
+    language?: String,
+    license?: {
+      name: String,
+    },
   }
 }
 
@@ -23,6 +28,23 @@ const ScrollItem = React.forwardRef(({
       rel="noreferrer">
       {itemData.name}
     </a>
+    <span className="infinite-scroll__item__desc">
+      {itemData.description}
+    </span>
+    <div className="infinite-scroll__item__remark-wrapper">
+      <div className="infinite-scroll__item__remark">
+        <i className="fa fa-star" />
+        {' '}
+        {itemData?.stargazers_count}
+      </div>
+      <div className="infinite-scroll__item__remark">
+        <div className="infinite-scroll__item__remark__circle-icon" />
+        {itemData?.language}
+      </div>
+      <div className="infinite-scroll__item__remark">
+        {itemData?.license?.name}
+      </div>
+    </div>
   </div>
 ));
 
